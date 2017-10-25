6689aec9fc297e19a02e95a46ebea254dc84a636
wmpscc <1214490813@qq.com>
2017-10-25 19:25:21 +0800

    实现高斯模糊与RecycleView透明显示。

--- app/src/main/java/com/wmpscc/alphaview/AlphaRecycleView.java
+++ app/src/main/java/com/wmpscc/alphaview/AlphaRecycleView.java
@@ -1,14 +1,13 @@
 package com.wmpscc.alphaview;
 
-import android.content.Context;
-import android.net.Uri;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import com.qiushui.blurredview.BlurredView;
 import me.drakeet.multitype.Items;
 import me.drakeet.multitype.MultiTypeAdapter;
 
@@ -19,7 +18,7 @@ public class AlphaRecycleView extends Fragment {
     private MultiTypeAdapter adapter;
     private View root;
     private Items items;
-
+    private BlurredView mBlurredView;
 
     public AlphaRecycleView() {
         // Required empty public constructor
@@ -30,12 +29,21 @@ public class AlphaRecycleView extends Fragment {
         super.onCreate(savedInstanceState);
 
     }
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         root=inflater.inflate(R.layout.fragment_alpha_recycle_view,container,false);
-        recyclerView=root.findViewById(R.id.rvlist);
-        adapter=new MultiTypeAdapter();
+        initview();
+        initRecycleView();
+
+        return root;
+    }
+
+    private void initRecycleView()
+
+    {adapter=new MultiTypeAdapter();
         adapter.register(card.class,new cardViewBinder());
         items=new Items();
         recyclerView.setAdapter(adapter);
@@ -47,7 +55,11 @@ public class AlphaRecycleView extends Fragment {
         adapter.setItems(items);
         adapter.notifyDataSetChanged();
 
-        return root;
+    }
+    private void initview(){
+        recyclerView=root.findViewById(R.id.rvlist);
+        mBlurredView=root.findViewById(R.id.blurredview);
+        mBlurredView.setBlurredLevel(100);
     }
 }
 
